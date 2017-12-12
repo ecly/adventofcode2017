84a4a8d5ec12039d9d1046b91e37b2072cbdd835
ecly <ecly@mailbox.org>
2017-12-12 14:10:43 +0100

    finished second day of half 11

--- day11/hex.hs
+++ day11/hex.hs
@@ -2,24 +2,28 @@ import Data.List.Split
 
 main :: IO()
 main = do 
-    input <- parseInput <$> readFile "input.in" 
+    input <- parseInput <$> getContents
     putStrLn $ show $ distance input
 
 parseInput :: String -> [String]
 parseInput = splitOn "," . head . lines
 
-distance :: [String] -> Integer
-distance = distance' (0,0)
+calcDist :: Integer -> Integer -> Integer
+calcDist x y = max (abs x) (abs y)
 
-distance' :: (Integer, Integer) -> [String] -> Integer
-distance' (x,y) [] = max (abs x) (abs y)
-distance' (x,y) (d:xs) = 
+distance :: [String] -> (Integer, Integer)
+distance = distance' (0,0) 0
+
+-- Returns Integer Tuple of (dist, maxDist)
+distance' :: (Integer, Integer) -> Integer -> [String] -> (Integer, Integer)
+distance' (x,y) z [] = (calcDist x y, z)
+distance' (x,y) z (d:xs) = 
     case d of 
-      "n"  -> distance' (x,y+1)   xs
-      "s"  -> distance' (x,y-1)   xs
-      "ne" -> distance' (x+1,y+1) xs
-      "se" -> distance' (x+1,y-1) xs
-      "nw" -> distance' (x-1,y+1) xs
-      "sw" -> distance' (x-1,y-1) xs
-      _    -> distance' (x,y)     xs
+      "n"  -> distance' (x,y+1)   (max (calcDist x y) z) xs
+      "s"  -> distance' (x,y-1)   (max (calcDist x y) z) xs
+      "ne" -> distance' (x+1,y+1) (max (calcDist x y) z) xs
+      "se" -> distance' (x+1,y-1) (max (calcDist x y) z) xs
+      "nw" -> distance' (x-1,y+1) (max (calcDist x y) z) xs
+      "sw" -> distance' (x-1,y-1) (max (calcDist x y) z) xs
+      _    -> distance' (x,y)     (max (calcDist x y) z) xs
 
