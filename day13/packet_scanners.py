ba3b3619f80688ac9f235b0d3b63f5b751491174
ecly <ecly@mailbox.org>
2017-12-13 10:39:49 +0100

    finished day 13

--- day13/packet_scanners.py
+++ day13/packet_scanners.py
@@ -1,14 +1,25 @@
-def main(file):
+def read(file):
     firewall = []
     for line in file:
         layer, range = map(str.strip, line.split(':'))
         firewall.append((int(layer), int(range)))
+    return firewall
         
+def ride(firewall):
     severity = 0
     for _, (l,r) in enumerate(firewall):
-        if l%((r-1)*2) == 0: severity += l*r
-
+        if l%(r*2-2) == 0: severity += l*r
     return severity
 
-with open('input.in', 'r') as file:
-    print(main(file))
+def is_caught(firewall, delay):
+    for _, (l,r) in enumerate(firewall):
+        if (l+delay)%(r*2-2) == 0: return True
+    return False
+
+file = open('input.in', 'r')  
+firewall = read(file)
+print(ride(firewall))
+
+delay = 0
+while is_caught(firewall, delay): delay+=1
+print(delay)
