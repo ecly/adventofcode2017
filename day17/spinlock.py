7dd78102b6a9f0adbc620720ce3242df071ee13c
ecly <ecly@mailbox.org>
2017-12-17 19:26:04 +0100

    finished second half of day 17

--- day17/spinlock.py
+++ day17/spinlock.py
@@ -1,10 +1,22 @@
-def main(step):
+def first(step):
     buffer = [0]
     pos = 0
     for i in range(1,2018):
-        pos = (pos + step) % len(buffer) + 1
+        pos = (pos + step) % i + 1
         buffer.insert(pos, i)
 
     return buffer[(pos+1)%len(buffer)]
 
-print(main(337))
+def second(step):
+    pos = 0
+    val = 0
+    for i in range(1,50_000_000):
+        pos = (pos + step) % i + 1
+        if pos == 1: 
+            val = i
+
+    return val
+
+input = 337
+print(first(input))
+print(second(input))
